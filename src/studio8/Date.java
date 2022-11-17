a3c67780a85c26e91573f9a31b61869ff89e5daa
lihongyu <lihongyu@localhost>
2022-11-17 12:46:18 -0600

    aa

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,59 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
+import org.junit.platform.commons.util.ToStringBuilder;
+
 public class Date {
+private int month;
+private int day;
+private int year;
+private boolean holiday;
 
+public Date(int month, int day,int year, boolean holiday) {
+	this.month=month;
+	this.day=day;
+	this.year=year;
+	this.holiday=holiday;
+	
+}
+@Override
+public int hashCode() {
+	return Objects.hash(day, month, year);
+}
 
-    public static void main(String[] args) {
-
+    @Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
+    
+    public String toString() {
+    	return day + " " + month +" " + year;
     }
-
+	public static void main(String[] args) {
+		LinkedList<Date> list = new LinkedList<Date>();
+		Date d1 = new Date(1, 2, 2022, false);
+		Date d2 = new Date(1, 2, 2022, false);
+		list.add(d1);
+		list.add(d2);
+		list.add(d1);
+		System.out.println(list);
+		HashSet<Date> set = new HashSet<Date>();
+		set.add(d1);
+		set.add(d2);
+		set.add(d1);
+		System.out.println(set);
+    }
+	
+	
+    
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,7 +1,34 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+private int hour;
+private int min;
+private int sec;
+
+public Time (int hour, int min, int sec) {
+	this.hour = hour;
+	this.min = min;
+	this.sec = sec;
+}
+
+	@Override
+public int hashCode() {
+	return Objects.hash(hour, min, sec);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Time other = (Time) obj;
+	return hour == other.hour && min == other.min && sec == other.sec;
+}
 
 	public static void main(String[] args) {
  
